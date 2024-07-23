8a27198955aed40aeb447ebaf2eee8c95aaf5403
nobelium24 <ogunbaja07@gmail.com>
2024-07-23 12:31:37 +0100

    web sockets

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ProductRoutes = require("./routes/ProductRoutes")
 const connectDb = require('./config/dbconfig')
 const productrouter = require("./routes/ProductRoutes")
 const cors = require("cors");
+const socketIo = require("socket.io");
 
 app.use(cors({origin:"*"}));
 app.use(express.urlencoded({ extended: true, limit:"200mb" }));
@@ -19,6 +20,27 @@ app.use(productrouter);
 connectDb()
 
 
-app.listen(4000, ()=>{
+const server = app.listen(4000, ()=>{
     console.log("App is running on port 4000");
+});
+
+const io = new socketIo.Server(server, {
+    cors: {
+        origin:"*"
+    }
+});
+
+
+//Emit: Used to send messages in the socket connection
+//On: Used to listen to messages in the socket connection
+io.on("connection", (socket)=>{
+    console.log(`user with id ${socket.id} is connected.`);
+    socket.on("message", (message)=>{
+        console.log(message);
+        io.emit("broadcast", message)
+    })
+
+    socket.on("disconnect", ()=>{
+        console.log(`user with id ${socket.id} is disconnected.`);
+    })
 })
